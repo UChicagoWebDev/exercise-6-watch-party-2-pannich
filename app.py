272e73184e02f6f9a7686f82014baa0768337107
pannich <pan.nichada@gmail.com>
2024-02-15 17:22:21 -0600

    submit 1

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import string
 import random
 from datetime import datetime
-from flask import Flask, g
+from flask import *
 from functools import wraps
+import sqlite3
+
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -29,20 +31,39 @@ def query_db(query, args=(), one=False):
     db.commit()
     cursor.close()
     if rows:
-        if one: 
+        if one:
             return rows[0]
         return rows
     return None
 
 def new_user():
+    # This generate random user name, password, api_key
     name = "Unnamed User #" + ''.join(random.choices(string.digits, k=6))
     password = ''.join(random.choices(string.ascii_lowercase + string.digits, k=10))
     api_key = ''.join(random.choices(string.ascii_lowercase + string.digits, k=40))
-    u = query_db('insert into users (name, password, api_key) ' + 
+    u = query_db('insert into users (name, password, api_key) ' +
         'values (?, ?, ?) returning id, name, password, api_key',
         (name, password, api_key),
         one=True)
     return u
+
+def get_user_from_api_key(request):
+    """Parse Request Header for api_key
+
+    Args:
+        request (_type_): post request
+    Returns:
+        user object: a sqlite row object of that user
+    """
+    api_key = request.headers.get('X-API-Key')
+    if api_key:
+        return query_db('select * from users where api_key = ?', [api_key], one=True)
+    return None
+
+def get_user_from_id(user_id):
+    """return user object
+    """
+    return query_db('select * from users where id = ?', [user_id], one=True)
 
 # TODO: If your app sends users to any other routes, include them here.
 #       (This should not be necessary).
@@ -62,15 +83,252 @@ def page_not_found(e):
 
 # -------------------------------- API ROUTES ----------------------------------
 
+# Authenticate USER API
+def require_api_key(f):
+    """a decorator function to check user api key
+    Check if the requested API header 'X-API-Key' matches with current user html cookie.
+    """
+    def decorated_function(*args, **kwargs):
+        api_key = request.headers.get('X-API-Key')
+        user = get_user_from_api_key(request)
+        if api_key and user: # There's X-API-KEY in header and user matches this key exists
+            print("API check: " + api_key, user, user['api_key'])
+            return f(*args, **kwargs)
+        else:
+            return jsonify({"error": "Invalid or missing API key."}), 400
+    decorated_function.__name__ = f.__name__
+    return decorated_function
+
 # TODO: Create the API
 
-# @app.route('/api/signup')
-# def login():
-#   ...
+#-------------------------------------------------
+#----------------sign-up login -------------------
+
+# api for signing up -> return API-KEY
+@app.route('/api/signup', methods=['POST']) #TODO create GET for when existing user /signup
+def signup():
+    """User coming in without API-KEY. Sign up initialize random username, password, API-KEY
+
+    Returns:
+        _type_: _description_
+    """
+    print("signup")
+    user = get_user_from_api_key(request)
+
+    if request.method == 'POST':
+        user = new_user() #generate random name, pw, api_key
+        print("user")
+        print(user)
+        for key in user.keys():
+            print(f'{key}: {user[key]}')
+
+        return jsonify({
+        'username': user['name'],
+        'password': user['password'],
+        'api_key': user['api_key']
+    }), 200
 
 
-# @app.route('/api/login')
-# def login():
-#   ... 
+@app.route('/api/login', methods=['POST'])
+def login():
+    """Browswer coming in without API-KEY. Log in finds API-KEY that matches input username, password
+"""
+    print("login")
 
-# ... etc
+    data = request.json
+    user_name = data.get('user_name')
+    password = data.get('password')
+
+    user = query_db('select * from users where name = ? and password = ?', [user_name, password], one=True)
+    if user: # found user
+        return jsonify({
+            'username': user['name'],
+            'password': user['password'],
+            'api_key': user['api_key']}), 200
+    else:
+        return jsonify({"message": "Invalid Username or Password"}), 401
+
+#-------------------------------------------------
+## all other /api requires @require_api_key
+
+
+@app.route('/api/user/credential', methods=['GET'])
+def get_credential():
+    user = get_user_from_api_key(request)
+    return jsonify(dict(user))
+
+# POST to change the user's name
+@app.route('/api/user/name', methods=['POST'])
+@require_api_key
+def update_username():
+    user = get_user_from_api_key(request)
+
+    if not user: return jsonify({'error': 'User not found'}), 403
+
+    # get new name
+    if request.method == 'POST':
+        if request.is_json:
+            data = request.get_json()
+            new_user_name = data.get('user_name')
+        try:
+            new_user = query_db('''
+        UPDATE users
+        SET name = ?
+        WHERE id = ? RETURNING *
+        ''', (new_user_name, user['id']), one=True)
+            return jsonify({'message': f"Username: {new_user['name']} updated successfully"}), 200
+        except Exception as e:
+            return jsonify({'error': 'Internal Server Error', 'details': str(e)}), 500
+
+# POST to change the user's password
+@app.route('/api/user/password', methods=['POST'])
+@require_api_key
+def update_password():
+    user = get_user_from_api_key(request)
+    if not user: return jsonify({'error': 'User not found'}), 403
+
+    # get new password
+    if request.method == 'POST':
+        if request.is_json:
+            data = request.get_json()
+            new_password = data.get('password')
+        try:
+            new_user = query_db('''
+        UPDATE users
+        SET password = ?
+        WHERE id = ? RETURNING *
+        ''', (new_password, user['id']), one=True)
+            return jsonify({'message': f"Password: {new_user['password']} updated successfully"}), 200
+        except Exception as e:
+            return jsonify({'error': 'Internal Server Error', 'details': str(e)}), 500
+
+# GET all rooms
+@app.route('/api/rooms', methods=['GET'])
+def get_rooms():
+    try:
+        rooms = query_db('select * from rooms', one=False)
+        if not rooms:
+            return jsonify({'message': 'no room found'}), 200
+        ls_rooms = []
+        for room in rooms:
+            ls_rooms.append({'id': room['id'], 'name': room['name']})
+        return jsonify(ls_rooms), 200
+    except Exception as e:
+        return jsonify({'error': f'An error occurred while fetching messages: {e}'}), 500
+
+# POST create new room
+@app.route('/api/rooms/new', methods=['POST'])
+@require_api_key
+def create_room():
+    print("create room") # For debugging
+    user = get_user_from_api_key(request)
+    if user is None: return {}, 403
+
+    if (request.method == 'POST'):
+        name = "Unnamed Room " + ''.join(random.choices(string.digits, k=6))
+        room = query_db('''
+    INSERT INTO rooms (name)
+    VALUES (?) RETURNING *
+    ''', (name,), one=True)
+        return jsonify({
+            'id': room['id'],
+            'name': room['name'],
+            }), 200
+    else:
+        return app.send_static_file('create_room.html')
+
+@app.route('/api/room/<int:room_id>', methods=['GET'])
+@require_api_key
+def get_room(room_id):
+
+    room = query_db('SELECT * FROM rooms WHERE id = ?', [room_id], one=True)
+    if room:
+        return jsonify(dict(room)), 200
+    else:
+        return jsonify({'message': 'Room not found'}), 404
+
+# POST to change the name of a room
+@app.route('/api/rooms/changename', methods=['POST'])
+@require_api_key
+def change_room_name():
+    """
+    json POST:
+    - room_id
+    - new_room_name
+
+    Returns:
+        json: _description_
+    """
+    if request.is_json:
+        data = request.get_json()
+        room_id = data.get('room_id')
+        new_room_name = data.get('new_room_name')
+    # Add your database logic here to update the room name
+    try:
+        room = query_db('''
+        UPDATE rooms
+        SET name = ?
+        WHERE id = ? RETURNING *
+        ''', (new_room_name, room_id), one=True)
+        return jsonify({'message': f"Room Name: {room['name']} updated successfully"}), 200
+    except Exception as e:
+        return jsonify({'error': 'Internal Server Error', 'details': str(e)}), 500
+
+# GET to get all the messages in a room
+@app.route('/api/messages', methods=['GET'])
+@require_api_key
+def get_messages():
+    room_id = request.args.get('room_id')  # Assuming you pass room_id as a query parameter
+    if not room_id:
+        return jsonify({'error': 'Room ID is required'}), 400
+
+    try:
+        messages = query_db('select * from messages where room_id = ?', [room_id], one=False)
+        # print(room_id, messages)
+        if not messages:
+            return jsonify({'message': 'no message found'}), 200
+        ls_messages = []
+        for message in messages:
+            user = get_user_from_id(message['user_id'])
+            if not user:
+                print("user id not found")
+                continue
+            ls_messages.append({'id': message['id'], 'user_name': user['name'], 'body': message['body']})
+        # print(ls_messages)
+        return jsonify(ls_messages), 200
+    except Exception as e:
+        return jsonify({'error': f'An error occurred while fetching messages: {e}'}), 500
+
+# POST to post a new message to a room
+@app.route('/api/messages/post', methods=['POST'])
+@require_api_key
+def post_message():
+    """
+    json POST:
+    header: 'X-API-Key'
+    body:
+    - room_id
+
+    Return:
+        json: {'user_name': user['name'], 'body': message['body']}
+    """
+    print("post message")
+    user = get_user_from_api_key(request)
+    user_id = user['id']
+    if request.is_json:
+        data = request.get_json()
+        body = data.get('body')
+        room_id = data.get('room_id')
+    if not room_id:
+        return jsonify({'error': 'Room ID is required'}), 400
+
+    message = query_db('''
+    INSERT INTO messages (user_id, room_id, body)
+    VALUES (?, ?, ?) RETURNING user_id, room_id, body
+    ''', (user_id, room_id, body), one=True)
+
+    try:
+        return jsonify({'user_name': user['name'], 'body': message['body']}), 200
+    except Exception as e:
+        # Log the exception e
+        return jsonify({'error': 'An error occurred while fetching messages'}), 500
--- static/script.js
+++ static/script.js
@@ -26,6 +26,8 @@ const checkPasswordRepeat = () => {
 passwordField.addEventListener("input", checkPasswordRepeat);
 repeatPasswordField.addEventListener("input", checkPasswordRepeat);
 
+let CURRENT_ROOM = 0;
+
 // TODO:  On page load, read the path and whether the user has valid credentials:
 //        - If they ask for the splash page ("/"), display it
 //        - If they ask for the login page ("/login") and don't have credentials, display it
@@ -52,4 +54,495 @@ repeatPasswordField.addEventListener("input", checkPasswordRepeat);
 //        seconds. When the user leaves the room, cancel that process.
 //        (Hint: https://developer.mozilla.org/en-US/docs/Web/API/setInterval#return_value)
 
+
+// -----------------------------------------
+// ----------- User functions ---------
+
+// function hasValidCredentials(params) {
+//   // This function should check for valid credentials.
+//   // For simplicity, we'll check Local Storage, but in a real app,
+//   // this might involve checking a cookie or making an API call.
+//   return localStorage.getItem('api_key') === 'valid'; // TODO : edit this with signup stuffs
+// }
+
+async function oneClickSignup() {
+  console.log("one click sign up");
+  try {
+    // Call your API to signup and get an API key
+    const response = await fetch('/api/signup', {
+      method: 'POST',
+      // Include any necessary headers or body according to your API
+    });
+    const user = await response.json();
+    console.log(user.api_key);
+
+    if (user.api_key) {
+      // Store the API key in Local Storage
+      localStorage.setItem('api_key', user.api_key);
+      localStorage.setItem('user_name', user.username);
+      localStorage.setItem('password', user.password);
+      alert('Signup successful! API key stored.');
+      navigateTo('/');
+    }
+  } catch (error) {
+    console.error('Error during signup:', error);
+  }
+}
+
+function logout() {
+  localStorage.removeItem('api_key');
+  localStorage.removeItem('user_name');
+  localStorage.removeItem('password');
+  window.location.reload();
+}
+
+async function login() {
+  try {
+    // Call your API to signup and get an API key
+    var user_name = document.querySelector('.alignedForm.login input[name="username"]').value;
+    var password = document.querySelector('.alignedForm.login input[name="password"]').value;
+
+    const response = await fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        "user_name": user_name,
+        "password": password
+      }),
+    });
+
+    const user = await response.json();
+    console.log(user.api_key); // extract api_key
+
+    // Displaying Credential Error Message
+    const failedElement = document.getElementById('failed-login-msg');
+
+    if (user.api_key) {
+      // Store the API key in Local Storage
+      console.log("test2")
+      localStorage.setItem('api_key', user.api_key);
+      localStorage.setItem('user_name', user.username);
+      localStorage.setItem('password', user.password);
+      alert('Login successful! API key stored.');
+      failedElement.classList.add("hide");
+      navigateTo('/');
+    } else {
+      failedElement.classList.remove("hide");
+    }
+  } catch (error) {
+    console.error('Error during login:', error);
+  }
+}
+
+// ----------- Room functions ---------
+
+// --- '/'
+async function createRoom() {
+  var user_name = document.querySelector('.alignedForm.login input[name="username"]').value;
+  var password = document.querySelector('.alignedForm.login input[name="password"]').value;
+  const X_API_Key = localStorage.getItem('api_key');
+
+  try {
+    const response = await fetch('/api/rooms/new', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-Key': X_API_Key // From global
+      }
+    });
+
+    const room = await response.json();
+    console.log(room.name); // debug
+    CURRENT_ROOM = room.id
+
+    if (room){
+      navigateTo(`/room/${CURRENT_ROOM}`)
+    }
+
+  } catch (error) {
+    console.error('Error during createRoom:', error);
+  }
+}
+
+async function displayRooms() {
+  let rooms;
+  try{
+    const response = await fetch('/api/rooms', {
+      method: 'GET',
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    rooms = await response.json();
+  } catch (error) {
+    console.error('Error during displayRooms:', error);
+    return;
+  }
+
+  var roomList = document.body.querySelector(".roomList");
+  roomList.innerHTML = '';
+  const noRooms = document.querySelector('.noRooms');
+  noRooms.classList.add("hide");
+
+  if (rooms.length === 0 || ! Array.isArray(rooms)){ // empty list
+    // display 'no room yet ... '
+    noRooms.classList.remove("hide")
+  } else {
+    rooms.forEach(room => {
+      const newRoomElement = document.createElement("a");
+      newRoomElement.textContent = `${room.id}: ${room.name}`;
+      newRoomElement.href = `#/room/${room.id}`; // Setting deep link
+      newRoomElement.addEventListener('click', function(event) {
+        event.preventDefault();
+        navigateTo(`/room/${room.id}`);
+      });
+      roomList.appendChild(newRoomElement);
+
+    });
+  }
+  return;
+}
+
+
+// -------------------------- PROFILE -----------------------------------
+// --- '/profile'
+async function updateCredential() {
+  const X_API_Key = localStorage.getItem('api_key');
+  try {
+    const response = await fetch(`/api/user/credential`, {
+      method: 'GET',
+      headers: {
+        'X-API-Key': X_API_Key
+      }
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    user = await response.json();
+    localStorage.setItem('user_name', user.name);
+    localStorage.setItem('password', user.password);
+
+  } catch (error) {
+    console.error('Failed to fetch messages:', error);
+  }
+  return user;
+}
+
+// --- username
+async function updateUserName() {
+  const X_API_Key = localStorage.getItem('api_key');
+  var new_user_name = document.querySelector('.auth input[name="username"]').value;
+
+  try {
+    const response = await fetch(`/api/user/name`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-Key': X_API_Key
+      },
+      body: JSON.stringify({
+        "user_name": new_user_name
+      }),
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    resp_message = await response.json();
+
+    await updateCredential(); // update localStorage
+    updateUserNameUI()
+
+  } catch (error) {
+    console.error('Failed to fetch messages:', error);
+  }
+  return;
+}
+
+async function updatePassword() {
+  const X_API_Key = localStorage.getItem('api_key');
+  var new_password = document.querySelector('.auth input[name="password"]').value;
+  try {
+    const response = await fetch(`/api/user/password`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-Key': X_API_Key
+      },
+      body: JSON.stringify({
+        "password": new_password
+      }),
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    resp_message = await response.json();
+
+    await updateCredential(); // update localStorage
+
+  } catch (error) {
+    console.error('Failed to fetch messages:', error);
+  }
+  return;
+}
+
+
+// -------------------------- ROOM -----------------------------------
+// ------ '/room/<int>'
+async function showRoom() {
+  const X_API_Key = localStorage.getItem('api_key');
+  let room;
+  try {
+    const response = await fetch(`/api/room/${CURRENT_ROOM}`, {
+      method: 'GET',
+      headers: {
+        'X-API-Key': X_API_Key
+      },
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    room = await response.json();
+  } catch (error) {
+    console.error('Error during showRoom:', error);
+    return;
+  }
+  // update roomName
+  const roomName = document.querySelector('.displayRoomName strong');
+  roomName.textContent = room.name;
+
+  const inviteLink = document.querySelector('#inviteLink');
+  inviteLink.href = `/room/${room.id}`;
+  inviteLink.textContent = `/room/${room.id}`;
+
+  // first load
+  getMessages();
+}
+
+async function updateRoomName() {
+  console.log("update room name")
+  var new_room_name = document.querySelector('.editRoomName input').value;
+  const X_API_Key = localStorage.getItem('api_key');
+
+  try {
+    const response = await fetch(`/api/rooms/changename`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-Key': X_API_Key
+      },
+      body: JSON.stringify({
+        "room_id": CURRENT_ROOM,
+        "new_room_name": new_room_name
+      }),
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    resp_message = await response.json();
+    // console.log(resp_message);
+
+    document.querySelector('.displayRoomName strong').textContent = new_room_name; // Update the display name instantly
+    this.closest('.editRoomName').classList.add('hide');
+    document.querySelector('.displayRoomName .material-symbols-outlined').classList.remove("hide");
+
+  } catch (error) {
+    console.error('Failed to fetch messages:', error);
+  }
+  return;
+}
+
+// -------- messages ----------
+async function postMessage() {
+  const X_API_Key = localStorage.getItem('api_key');
+
+  const bodyValue = document.querySelector('textarea[name="comment"]').value;
+
+  let new_message
+  try {
+    const response = await fetch(`/api/messages/post`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-API-Key': X_API_Key
+      },
+      body: JSON.stringify({
+        "body": bodyValue,
+        "room_id": CURRENT_ROOM,
+      }),
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    new_message = await response.json();
+    console.log(new_message);
+  } catch (error) {
+    console.error('Failed to fetch messages:', error);
+  }
+  return;
+}
+
+async function getMessages() {
+  const X_API_Key = localStorage.getItem('api_key');
+  let messages;
+  try {
+    const response = await fetch(`/api/messages?room_id=${CURRENT_ROOM}`, {
+      method: 'GET',
+      headers: {
+        'X-API-Key': X_API_Key
+      },
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    messages = await response.json();
+    console.log(messages); //debug
+  } catch (error) {
+    console.error('Failed to fetch messages:', error);
+  }
+
+  var resultsContainer = document.body.querySelector(".messages");
+
+  // clear sample messages
+  resultsContainer.innerHTML = '';
+
+  if (! Array.isArray(messages)) {
+    return;
+  } else {
+    messages.forEach(message => {
+      // update a new message
+      const newMsgClass = document.createElement("message");
+
+      const authorElement = document.createElement("author");
+      authorElement.textContent = message["user_name"];
+      const contentElement = document.createElement("content");
+      contentElement.textContent = message["body"];
+
+      newMsgClass.appendChild(authorElement);
+      newMsgClass.appendChild(contentElement);
+
+      resultsContainer.appendChild(newMsgClass);
+   });
+  }
+  return;
+}
+
+// -------- navigation ---------
+
+// Function to navigate to a room
+// https://developer.mozilla.org/en-US/docs/Web/API/History/pushState#examples:~:text=in%20its%20hash.-,Examples,-This%20creates%20a
+function navigateTo(path) {
+  // Triggered by html onclick
+  // stateObject : Whenever the user navigates to the new state, a popstate event is fired, and the state property of the event contains a copy of the history entry's state object.
+  const stateObject = { path: path };
+  console.log(`Navigating to ${path}`);
+
+  // Use the History API to update the URL and push the new state to the history
+  history.pushState(stateObject, '', path);
+  window.dispatchEvent(new Event('popstate')); // SPA: Update to new url wo reloading
+}
+
+
+// -------- pages management --------
 // On page load, show the appropriate page and hide the others
+
+// show me a new "page"
+let showOnly = (element) => {
+  SPLASH.classList.add("hide");
+  LOGIN.classList.add("hide");
+  ROOM.classList.add("hide");
+  PROFILE.classList.add("hide");
+
+  element.classList.remove("hide");
+}
+
+let router = () => {
+  let path = window.location.pathname;
+
+  switch(true) { // switch is the same as if-else
+    case path === "/":
+      console.log("get / page");
+      // show the index
+      CURRENT_ROOM = 0;
+      showOnly(SPLASH);
+      break;
+    case path === "/profile":
+      // show the profile
+      showOnly(PROFILE);
+      break;
+    case path.startsWith("/room"):
+      showOnly(ROOM);
+      const roomId = path.split("/")[2]; // This is a simple way to get the room ID
+      console.log("Room ID:", roomId);
+
+      CURRENT_ROOM = roomId;
+      showRoom(); // load based on CURRENT_ROOM
+
+      break;
+    case path === "/login":
+      showOnly(LOGIN);
+      break;
+    //else do 404
+    default:
+      console.log(`404 Page Not Found ${path}`);
+      // Show 404 page or redirect to a default page
+      break;
+  }
+}
+
+
+// TODO : Make all HTTP requests after the page load with fetch calls to API endpoints that return JSON. Prefix API routes with /api. ??
+// TODO : Opening /, /login, or /profile in a new browser window opens the app to those screens. ????
+// do the {{username}} update
+
+// ----------- UI ------------
+// --------- Update {{Username}}
+function updateUserNameUI() {
+  var user_name = '';
+  user_name = localStorage.getItem('user_name');
+  const allUserNameUI = document.querySelectorAll('.username');
+  allUserNameUI.forEach(element => {
+    element.textContent = user_name;
+  });
+}
+
+// --------- Toggle EditRoomIcon
+function editRoomToggle() {
+
+}
+
+
+window.addEventListener("DOMContentLoaded", () => {
+  updateUserNameUI();
+  router();
+  displayRooms();
+
+  // edit room name toggle
+  const editIcon = document.querySelector('.displayRoomName .material-symbols-outlined');
+  const editroomname = document.querySelector('.editroomname');
+  if (editIcon) {
+    editIcon.addEventListener('click', () => {
+      editroomname.classList.remove("hide");
+      editIcon.classList.add("hide");
+    });
+  }
+  const saveIcon = document.querySelector('.editroomname button');
+  if (saveIcon) {
+    saveIcon.addEventListener('click', updateRoomName);
+  }
+
+  setInterval(async () => {
+    if (CURRENT_ROOM == 0) {
+      return;
+    }
+    // poll messages
+    console.log("polling");
+    await getMessages();
+
+  }, 5000);
+
+
+});
+
+window.addEventListener('popstate', router);
